c2c42a33d383c88dfc186264a4afeeb62f110a83
dhruvu340 <dhruvu3400@gmail.com>
2026-05-21 15:32:20 +0530

    vectorise logic done

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -4,6 +4,10 @@ import {Pinecone} from "@pinecone-database/pinecone"
 import {Document,RecursiveCharacterTextSplitter} from "@pinecone-database/doc-splitter"
 import { downloadFromS3 } from "./s3-server";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { getEmbeddings } from "./embeddings";
+import { Vector } from "@pinecone-database/pinecone/dist/pinecone-generated-ts-fetch/db_data";
+import md5 from "md5"
+
 export const pc = new Pinecone({
   apiKey: process.env.PINECONE_API_KEY!,
 });
@@ -25,9 +29,25 @@ export async function loadS3IntoPinecone(fileKey:string){
     const loader = new PDFLoader(fileName);
     const pages = (await loader.load()) as PdfPage[];
     const documents = await Promise.all(pages.map(prepareDocument));
-    return documents;
+    const vectors = await Promise.all(documents.flat().map(embedDocuments));
 }
-
+export async function embedDocuments(doc:Document){
+    try {
+        const embeddings=await getEmbeddings(doc.pageContent);
+        const hash = md5(doc.pageContent);
+        return {
+            id : hash,
+            values:embeddings,
+            metadata:{
+                text:doc.metadata.text,
+                pageNumber:doc.metadata.pageNumber,
+            }
+        } as Vector;
+    } catch (error) {
+        console.log("error embedding document",error);
+        throw error;
+    }
+}
 export const truncateByteByByte=(str:string,bytes:number)=>{
 const enc = new TextEncoder;
 return new TextDecoder("utf-8").decode(enc.encode(str).slice(0,bytes));
